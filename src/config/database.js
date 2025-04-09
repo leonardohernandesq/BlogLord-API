4f22e25ec9a091f681096aca4c1daee63d03d4bb
Leonardo Hernandes <leonardo_hernandes@outlook.com.br>
2025-04-09 14:55:19 +0100

    build config

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI); // Remover as opções desnecessárias
     console.log("📦 Conectado ao MongoDB!");
   } catch (error) {
     console.error("❌ Erro ao conectar ao MongoDB:", error);
