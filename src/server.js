2d482bc20e2ec35a90558ee5fff3a94c711cafbf
Leonardo Hernandes <62108109+leonardohernandesq@users.noreply.github.com>
2025-05-28 18:51:25 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use("/api", Routes);
 
